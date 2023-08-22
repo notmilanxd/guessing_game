70f1b48f144aa93e2acfa46eaac0a87f95c3b382
notmilanxd <supermilan009@gmail.com>
2023-08-22 23:08:42 +0200

    pretty much done

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,38 @@
+use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("not milan original game: Guess the number digga!");
+    println!("not milan original game: Guess the number!");
+    println!("You can quit with ctrl+c but why would u do that-");
 
-    println!("Please input your guess 🙉");
+    let secret_number = rand::thread_rng().gen_range(0..=100);
 
-    let mut guess = String::new(); //mut makes it mutable aka it can be changed (wow)
+    loop {
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line hahah");
+        println!("Please input your guess 🙉");
 
-    println!("You guessed: {guess}");
+        let mut guess = String::new(); //mut makes it mutable aka it can be changed (wow)
 
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line hahah");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+            
+
+        println!("You guessed: {guess}");
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small! 🗿"),
+            Ordering::Equal => {
+                println!("✅ Wow you've got it right");
+                break;
+                }
+            Ordering::Greater => println!("Too big! 😩"),
+        }
+    }
 }
